ff5f4a6868efd74ce38235785126609794a9c14a
Jack May <jack@solana.com>
2019-02-15 09:33:11 -0800

    [BPF] Add support for relative relocations

--- llvm/lib/Target/BPF/MCTargetDesc/BPFELFObjectWriter.cpp
+++ llvm/lib/Target/BPF/MCTargetDesc/BPFELFObjectWriter.cpp
@@ -27,9 +27,21 @@ public:
 protected:
   unsigned getRelocType(MCContext &Ctx, const MCValue &Target,
                         const MCFixup &Fixup, bool IsPCRel) const override;
+
+  bool needsRelocateWithSymbol(const MCSymbol &Sym,
+                               unsigned Type) const override;
 };
 
 } // end anonymous namespace
+
+// Avoid section relocations because the BPF backend can only handle 
+// section relocations with values (offset into the section containing
+// the symbol being relocated).  Forcing a relocation with a symbol
+// will result in the symbol's index being used in the .o file instead.
+bool BPFELFObjectWriter::needsRelocateWithSymbol(const MCSymbol &Sym,
+                                                 unsigned Type) const {
+  return true;
+}
 
 BPFELFObjectWriter::BPFELFObjectWriter(uint8_t OSABI)
     : MCELFObjectTargetWriter(/*Is64Bit*/ true, OSABI, ELF::EM_BPF,
@@ -42,13 +54,6 @@ unsigned BPFELFObjectWriter::getRelocType(MCContext &Ctx, const MCValue &Target,
   switch (Fixup.getKind()) {
   default:
     llvm_unreachable("invalid fixup kind!");
-  case FK_SecRel_8:
-    return ELF::R_BPF_64_64;
-  case FK_PCRel_4:
-  case FK_SecRel_4:
-    return ELF::R_BPF_64_32;
-  case FK_Data_8:
-    return ELF::R_BPF_64_64;
   case FK_Data_4:
     if (const MCSymbolRefExpr *A = Target.getSymA()) {
       const MCSymbol &Sym = A->getSymbol();
@@ -79,7 +84,12 @@ unsigned BPFELFObjectWriter::getRelocType(MCContext &Ctx, const MCValue &Target,
         }
       }
     }
+  case FK_PCRel_4:
+  case FK_SecRel_4:
     return ELF::R_BPF_64_32;
+  case FK_SecRel_8:
+  case FK_Data_8:
+    return ELF::R_BPF_64_64;
   }
 }
 
