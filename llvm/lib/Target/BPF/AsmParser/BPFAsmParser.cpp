e5358217f8397df63b6bdb0412d329fead2df60c
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2021-12-03 10:18:25 -0800

    [SOL] Register SBF asm parser

--- llvm/lib/Target/BPF/AsmParser/BPFAsmParser.cpp
+++ llvm/lib/Target/BPF/AsmParser/BPFAsmParser.cpp
@@ -507,4 +507,5 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeBPFAsmParser() {
   RegisterMCAsmParser<BPFAsmParser> X(getTheBPFTarget());
   RegisterMCAsmParser<BPFAsmParser> Y(getTheBPFleTarget());
   RegisterMCAsmParser<BPFAsmParser> Z(getTheBPFbeTarget());
+  RegisterMCAsmParser<BPFAsmParser> XX(getTheSBFTarget());
 }
