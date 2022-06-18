5c98aeae7331ae94a980c1d903f61a8742d656ef
Richard Patel <richard@blockdaemon.com>
2022-06-18 05:46:20 +0100

    [SOL] Fix callx in AsmParser

--- llvm/lib/Target/BPF/AsmParser/BPFAsmParser.cpp
+++ llvm/lib/Target/BPF/AsmParser/BPFAsmParser.cpp
@@ -226,6 +226,7 @@ public:
     return StringSwitch<bool>(Name.lower())
         .Case("if", true)
         .Case("call", true)
+        .Case("callx", true)
         .Case("goto", true)
         .Case("*", true)
         .Case("exit", true)
