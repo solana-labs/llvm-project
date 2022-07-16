4006d04ca9ffdb8a8c525eff59d4e89a7fa75d5c
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2022-07-16 15:48:56 -0700

    [SOL] Update failing tests after upgrade

--- clang/test/CodeGen/ext-int-cc.c
+++ clang/test/CodeGen/ext-int-cc.c
@@ -65,7 +65,7 @@ void ParamPassing(_BitInt(128) b, _BitInt(64) c) {}
 // LA64: define{{.*}} void @ParamPassing(i128 %{{.+}}, i64 %{{.+}})
 // LA32: define{{.*}} void @ParamPassing(ptr %{{.+}}, i64 %{{.+}})
 // BPF: define{{.*}} void @ParamPassing(i128* byval(i128) align 8 %{{.+}}, i64 %{{.+}})
-// SBF: define{{.*}} void @ParamPassing(i128* byval(i128) align 8 %{{.+}}, i64 %{{.+}})
+// SBF: define{{.*}} void @ParamPassing(i128 %{{.+}}, i64 %{{.+}})
 
 void ParamPassing2(_BitInt(127) b, _BitInt(63) c) {}
 // LIN64: define{{.*}} void @ParamPassing2(i64 %{{.+}}, i64 %{{.+}}, i64 %{{.+}})
@@ -98,7 +98,7 @@ void ParamPassing2(_BitInt(127) b, _BitInt(63) c) {}
 // LA64: define{{.*}} void @ParamPassing2(i127 %{{.+}}, i63 signext %{{.+}})
 // LA32: define{{.*}} void @ParamPassing2(ptr %{{.+}}, i63 %{{.+}})
 // BPF: define{{.*}} void @ParamPassing2(i127* byval(i127) align 8 %{{.+}}, i63 %{{.+}})
-// SBF: define{{.*}} void @ParamPassing2(i127* byval(i127) align 8 %{{.+}}, i63 %{{.+}})
+// SBF: define{{.*}} void @ParamPassing2(i127 %{{.+}}, i63 %{{.+}})
 
 // Make sure we follow the signext rules for promotable integer types.
 void ParamPassing3(_BitInt(15) a, _BitInt(31) b) {}
