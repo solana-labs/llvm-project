a75aae58d35a96f10a54227664d5f5bcdac16d5a
Lucas Steuernagel <lucas.tnagel@gmail.com>
2023-12-20 13:28:00 -0300

    Remove lddw instruction

--- llvm/lib/Target/SBF/MCTargetDesc/SBFMCCodeEmitter.cpp
+++ llvm/lib/Target/SBF/MCTargetDesc/SBFMCCodeEmitter.cpp
@@ -93,17 +93,19 @@ unsigned SBFMCCodeEmitter::getMachineOpValue(const MCInst &MI,
   if (MI.getOpcode() == SBF::JAL)
     // func call name
     Fixups.push_back(MCFixup::create(0, Expr, FK_PCRel_4));
-  else if (MI.getOpcode() == SBF::LD_imm64)
+  else if (MI.getOpcode() == SBF::LD_imm64 ||
+           MI.getOpcode() == SBF::MOV_32_64_addr)
     Fixups.push_back(MCFixup::create(0, Expr, FK_SecRel_8));
-  else
+  else if (MI.getOpcode() == SBF::HOR_addr) {
+    // This is already handled in MOV_32_64
+  } else
     // bb label
     Fixups.push_back(MCFixup::create(0, Expr, FK_PCRel_2));
 
   return 0;
 }
 
-static uint8_t SwapBits(uint8_t Val)
-{
+static uint8_t SwapBits(uint8_t Val) {
   return (Val & 0x0F) << 4 | (Val & 0xF0) >> 4;
 }
 
