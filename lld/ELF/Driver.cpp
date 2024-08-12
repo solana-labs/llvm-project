0f67554dd1b903905353a967cdd56c3fcbb7a0d6
Lucas Steuernagel <lucas.tnagel@gmail.com>
2024-08-12 18:04:02 -0300

    [SOL] Do not check relocs for SBF

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -1754,7 +1754,8 @@ static void setConfigs(opt::InputArgList &args) {
   // builds and disabled otherwise. This check is enabled when writeAddends is
   // true.
 #ifndef NDEBUG
-  bool checkDynamicRelocsDefault = true;
+  // The SBF and BPF target for Solana do not support checking dynamic relocs.
+  bool checkDynamicRelocsDefault = m != EM_BPF && m != EM_SBF;
 #else
   bool checkDynamicRelocsDefault = false;
 #endif
